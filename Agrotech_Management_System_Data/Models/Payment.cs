a1e3bb27d18b8de36f237376916be536924ffada
abhi616 <ay891241@gmail.com>
2022-05-28 20:48:04 +0530

    Add Payment Model

---
+++ Agrotech_Management_System_Data/Models/Payment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agrotech_Management_System_Data.Models
+{
+   public class Payment
+    {
+        public int ID;
+    }
+}
